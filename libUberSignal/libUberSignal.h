936d0187a134688b41c2ffad9e0aecb435b3b1ba
xxxoo <xxxoo>
2020-12-15 22:08:02 -0800

    could publish

---
+++ libUberSignal/libUberSignal.h
@@ -0,0 +1,13 @@
+//
+//  libUberSignal.h
+//  libUberSignal
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Uber Technologies Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libUberSignal : NSObject
+
+@end
---
+++ libUberSignal/libUberSignal.m
@@ -0,0 +1,13 @@
+//
+//  libUberSignal.m
+//  libUberSignal
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Uber Technologies Inc. All rights reserved.
+//
+
+#import "libUberSignal.h"
+
+@implementation libUberSignal
+
+@end
